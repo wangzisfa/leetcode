2b125e576e251f71e7d094afadfcb1d2d83879b1
wangzisfa <729503433@qq.com>
2022-01-04 22:54:21 +0800

    feat: leetcode 204计数质数

---
+++ src/com/github/wangzisfa/LeetCode_204.java
@@ -0,0 +1,38 @@
+package com.github.wangzisfa;
+
+import java.util.Arrays;
+
+/**
+ * Created With IntelliJ IDEA
+ *
+ * @Author: wangzisfa
+ * @Date: 2021/09/08
+ * @Description:
+ */
+public class LeetCode_204 {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.countPrimes(10));
+	}
+
+	static class Solution {
+		public int countPrimes(int n) {
+			boolean[] isPrime = new boolean[n];
+			int sum = 0;
+			Arrays.fill(isPrime, true);
+
+			for (int i = 2; i < n; i++) {
+				if (isPrime[i]) {
+					sum++;
+					if ((long) i * i < n) {
+						for (int j = i * i; j < n; j += i) {
+							isPrime[j] = false;
+						}
+					}
+				}
+			}
+
+			return sum;
+		}
+	}
+}
