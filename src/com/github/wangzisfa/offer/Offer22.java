8a50d1804518e180d9c9a78ec070e1f9cd487075
wangzisfa <729503433@qq.com>
2022-01-12 23:10:40 +0800

    feat: 剑指Offer22 链表中倒数地k个节点(双指针写法)

--- src/com/github/wangzisfa/offer/Offer22.java
+++ src/com/github/wangzisfa/offer/Offer22.java
@@ -1,23 +1,41 @@
 package com.github.wangzisfa.offer;
 
 public class Offer22 {
+//	public ListNode getKthFromEnd(ListNode head, int k) {
+//		ListNode cur = head;
+//		int curNum = 1;
+//		int size = 1;
+//		while (cur != null) {
+//			cur = cur.next;
+//			size++;
+//		}
+//
+//		cur = head;
+//		while (cur != null) {
+//			if (curNum == size - k) return cur;
+//			//System.out.println(size - k + " " + curNum);
+//			cur = cur.next;
+//			curNum++;
+//		}
+//
+//		return cur;
+//	}
+
+
+	//双指针
 	public ListNode getKthFromEnd(ListNode head, int k) {
 		ListNode cur = head;
-		int curNum = 1;
-		int size = 1;
+		ListNode prev = head;
+		int tmp = 0;
 		while (cur != null) {
-			cur = cur.next;
-			size++;
+			if (tmp != k) {
+				cur = cur.next;
+				tmp++;
+			} else {
+				cur = cur.next;
+				prev = prev.next;
+			}
 		}
-
-		cur = head;
-		while (cur != null) {
-			if (curNum == size - k) return cur;
-			//System.out.println(size - k + " " + curNum);
-			cur = cur.next;
-			curNum++;
-		}
-
-		return cur;
+		return prev;
 	}
 }
