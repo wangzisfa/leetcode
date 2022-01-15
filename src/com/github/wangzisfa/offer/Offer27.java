bb127aba3c57e23100e1039ccfefe6c18e6ed5ca
wangzisfa <729503433@qq.com>
2022-01-16 00:00:19 +0800

    feat: 剑指Offer27 二叉树的镜像(辅助栈和更好的bfs写法)

--- src/com/github/wangzisfa/offer/Offer27.java
+++ src/com/github/wangzisfa/offer/Offer27.java
@@ -1,5 +1,7 @@
 package com.github.wangzisfa.offer;
 
+import java.util.Stack;
+
 /**
  * Description:
  * <p>
@@ -24,5 +26,33 @@ public class Offer27 {
         
         bfs(current.left);
         bfs(current.right);
+    }
+    
+    public TreeNode bfsBetter(TreeNode current) {
+        if (current == null) return null;
+        
+        TreeNode temp = current.left;
+        current.left = bfsBetter(current.right);
+        current.right = bfsBetter(temp);
+        
+        return current;
+    }
+    
+    // 辅助栈
+    public TreeNode withStack(TreeNode current) {
+        if (current == null) return current;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(current);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (current.left != null) stack.push(current.left);
+            if (current.right != null) stack.push(current.right);
+            // 这边再写的时候稍微有点看不懂, 感觉有点像是 node 暂存了当前节点的值
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+        
+        return current;
     }
 }
